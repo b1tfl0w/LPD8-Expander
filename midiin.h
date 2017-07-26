f79d5fc3ba83f422bc5cda92d2ab6b9640c69ca4
Sasha Bon <30445498+b1tfl0w@users.noreply.github.com>
2017-07-26 02:25:29 +0200

    Update midiin.h

--- midiin.h
+++ midiin.h
@@ -264,9 +264,6 @@ void mycallback(double deltatime, std::vector<unsigned char> *message, void * /*
     }
 }
 
-// This function should be embedded in a try/catch block in case of
-// an exception.  It offers the user a choice of MIDI ports to open.
-// It returns false if there are no ports available.
 bool chooseMidiIn(RtMidiIn *rtmidi);
 bool chooseMidiOut(RtMidiOut *rtmidi);
 bool chooseMidiOut2(RtMidiOut *rtmidi);
